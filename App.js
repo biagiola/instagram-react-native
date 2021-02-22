6b0b8b93272c28c11a0f0575c7fc2ce224a4f6ff
David <41311088+biagiola@users.noreply.github.com>
2021-02-22 20:28:38 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,16 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import Header from './components/header';
+import Header from './components/Header/Header';
 
-import Posts from './components/Posts';
+import Posts from './components/Posts/Posts';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <StatusBar style={{ color: '#212121'}} />
-      <Header />
-      <Posts/>
+    <View>
+      <View style={styles.container}>
+        <Header />
+        <Posts/>
+        <StatusBar style="light" backgroundColor="black" />
+      </View>
     </View>
   );
 }
@@ -19,5 +21,6 @@ const styles = StyleSheet.create({
   container: {
     flexDirection: 'column',
     backgroundColor: '#fafafa',
-  },
+    paddingBottom: 70
+  }
 });
